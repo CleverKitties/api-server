a95f70b9d0f4e538cd3e22107aa6805a05d63ad9
cleverkitties <drawings.crypto@gmail.com>
2021-12-08 02:17:18 +1000

    update signature

--- api/signature.js
+++ api/signature.js
@@ -14,7 +14,7 @@ const validateSignature = async (address, signature) => {
     address = toLowerCase(address);
     let user = await User.findOne({ address });
     let nonce = user.nonce;
-    let msg = `Confirm wallet ownership on CleverKitties : ${nonce}`;
+    let msg = `confirm kitty ownership on clever kitties with nonce : ${nonce}`;
     let msgBufferHex = ethUtil.bufferToHex(Buffer.from(msg, "utf8"));
     let _address = toLowerCase(
       sigUtil.recoverPersonalSignature({
